c4c7b099464e6a2879e12ba903c128328f0988ad
anso33 <hyeon131812@gmail.com>
2023-02-27 17:21:02 +0900

    FEAT: init project file

---
+++ todayit/src/main/java/com/juju/todayit/TodayitApplication.java
@@ -0,0 +1,13 @@
+package com.juju.todayit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodayitApplication.class, args);
+  }
+
+}
---
+++ todayit/src/test/java/com/juju/todayit/TodayitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juju.todayit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
