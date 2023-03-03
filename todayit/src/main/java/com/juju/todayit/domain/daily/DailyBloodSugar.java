ce688084de5fc31b9a81d86cd16e3ed7a971593a
anso33 <hyeon131812@gmail.com>
2023-03-04 08:51:23 +0900

    FEAT: feat entity of daily_blood_sugar table

---
+++ todayit/src/main/java/com/juju/todayit/domain/daily/DailyBloodSugar.java
@@ -0,0 +1,25 @@
+package com.juju.todayit.domain.daily;
+
+import com.juju.todayit.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class DailyBloodSugar {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  private double level;
+
+  private LocalDateTime test_time;
+}
