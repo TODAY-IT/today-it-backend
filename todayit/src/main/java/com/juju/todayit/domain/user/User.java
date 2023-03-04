89606a1663ffaafdc42c6574993e7afb795f06fa
anso33 <hyeon131812@gmail.com>
2023-03-04 09:38:57 +0900

    FEAT: feat entity of user table

---
+++ todayit/src/main/java/com/juju/todayit/domain/user/User.java
@@ -0,0 +1,59 @@
+package com.juju.todayit.domain.user;
+
+import com.juju.todayit.domain.daily.DailyBloodSugar;
+import com.juju.todayit.domain.daily.DailyMeal;
+import com.juju.todayit.domain.standard.DailyIntakeStandard;
+import com.juju.todayit.domain.user.userinfo.UserDisease;
+import com.juju.todayit.domain.user.userinfo.UserSupplement;
+import com.juju.todayit.domain.utils.enums.Gender;
+import com.juju.todayit.domain.utils.enums.PurposeOfUse;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+  @Column(nullable = false)
+  private Long id;
+
+  private String kakao_id;
+
+  private String name;
+
+  private int age;
+
+  private double height;
+
+  private double weight;
+
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  @Enumerated(EnumType.STRING)
+  private PurposeOfUse purpose;
+
+  @ManyToOne
+  @JoinColumn(name = "daily_intake_standard_id")
+  private DailyIntakeStandard dailyIntakeStandard;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<DailyBloodSugar> bloodSugarList = new ArrayList<>();
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserDisease> userDiseaseList = new ArrayList<>();
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<DailyMeal> dailyMealList = new ArrayList<>();
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserSupplement> userSupplementList = new ArrayList<>();
+}
