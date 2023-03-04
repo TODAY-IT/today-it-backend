32d8b48c0d8dfcc7a5351296bf27e2d0962784f8
anso33 <hyeon131812@gmail.com>
2023-03-04 09:39:15 +0900

    FEAT: feat entity of daily_intake_standard table

---
+++ todayit/src/main/java/com/juju/todayit/domain/standard/DailyIntakeStandard.java
@@ -0,0 +1,42 @@
+package com.juju.todayit.domain.standard;
+
+import com.juju.todayit.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "daily_intake_standard")
+public class DailyIntakeStandard {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private double total_calories;
+
+  private double carbohydrate;
+
+  private double protein;
+
+  private double fat;
+
+  private double saturated_fat;
+
+  private double trans_fat;
+
+  private double sugars;
+
+  private double sodium;
+
+  private double cholesterol;
+
+  private Long userId;
+
+  @OneToMany(mappedBy = "daily_intake_standard", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<User> userList = new ArrayList<>();
+}
