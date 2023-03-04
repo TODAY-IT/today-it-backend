6ec01aac1d5ac66596af5325d1d02eaea8c048f3
anso33 <hyeon131812@gmail.com>
2023-03-04 09:33:48 +0900

    FEAT: feat entity of meal_nutrition_facts table

---
+++ todayit/src/main/java/com/juju/todayit/domain/mealinfo/MealNutritionFacts.java
@@ -0,0 +1,40 @@
+package com.juju.todayit.domain.mealinfo;
+
+import com.juju.todayit.domain.daily.DailyMeal;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "meal_nutrition_facts")
+public class MealNutritionFacts {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(mappedBy = "meal_nutrition_facts")
+  @JoinColumn(name = "daily_meal_id")
+  private DailyMeal dailyMeal;
+//  private Long daily_meal_id;
+
+  private double total_calories;
+
+  private double carbohydrate;
+
+  private double protein;
+
+  private double fat;
+
+  private double saturated_fat;
+
+  private double trans_fat;
+
+  private double sugars;
+
+  private double sodium;
+
+  private double cholesterol;
+}
